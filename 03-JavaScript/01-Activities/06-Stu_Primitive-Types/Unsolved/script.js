1a6fda3c7141aeb22869b246fd4ecccb1ed69c1e
erin-maxson <erin@cohatch.com>
2022-05-08 14:52:13 -0500

    comments on variables and logs

--- 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
+++ 03-JavaScript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -1,16 +1,28 @@
+// variable #1 is a number (primitive data type//
 var one = 64;
+// variable #2 is a string //
 var two = "656302";
+// variable #3 is a boolean value //
 var three = false;
+// variable #4 is a number value //
 var four = 64.55;
+// variable #5 is a string that produces the word Howdy! //
 var five = "Howdy!";
+// variable #6 is undefined //
 var six;
 
 // Insert comments to explain what each console log below will log to the console
+// will log the number 5=64 //
 console.log(typeof one); 
+// will log the string 656302 //
 console.log(typeof two); 
+// will log a false value //
 console.log(typeof three); 
+// will log the number 64.55 //
 console.log(typeof four); 
+// will log the string "Howdy!" //
 console.log(typeof five);
+// will not log anything...it is undefined //
 console.log(typeof six);
 
 four = "Hello!";
