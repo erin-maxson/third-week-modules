b34bcd3e0a4b8027e39af183104da3a61ce088b5
erin-maxson <erin@cohatch.com>
2022-05-08 14:45:10 -0500

    creating variable values and logs

--- 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,28 @@
 // Write Your JavaScript Code Here
+
+//personName variable//
+var personName = "Erin";
+// # of pets variable //
+var pets = "2"
+// funFact variable //
+var funFact = "My two dogs and I live and travel in a van.";
+
+// // re-assigning variables //
+// personName =
+// pets =
+// funFact =
+
+//Access values stored in variable through the console log //
+console.log(personName);
+console.log(pets);
+console.log(funFact);
+
+// Combining variable values with a message, creating logs for messages //
+console.log("My name is ");
+
+//Logs for all variables //
+console.log("My name is " + personName);
+console.log("I have " + pets + "pets");
+console.log("Fun fact:" + funFact);
+
+
