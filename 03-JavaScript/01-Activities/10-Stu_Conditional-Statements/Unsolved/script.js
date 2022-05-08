4515dde17f4f6e9b8a18c06479848f310c6508b3
erin-maxson <erin@cohatch.com>
2022-05-08 15:11:25 -0500

    conditional statements in js

--- 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
+++ 03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -4,3 +4,23 @@ var expression1 = (x < 25);
 var expression2 = (x > 50);
 
 // Write Your JavaScript Code Here
+
+if (expression1 = 26) + (expression2 = 51)
+{ 
+    console.log("True ✅  True ✅ ");
+}
+
+if (expression1 = 26) 
+{
+    console.log("True ✅  False ❌")
+}
+
+if (expression2 = 51)
+{   
+    console.log("False ❌  True ✅ ")
+}
+
+if (expression1 = 24) + (expression2 = 49)
+{   
+    console.log("False ❌  False ❌")
+}
