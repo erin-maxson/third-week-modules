165f627feafb90324e2605dd8422b72200fa31c1
erin-maxson <erin@cohatch.com>
2022-05-09 20:04:29 -0500

    notes for script

--- 03-JavaScript/01-Activities/08-Stu_Logical-Comparison-Operators/Unsolved/script.js
+++ 03-JavaScript/01-Activities/08-Stu_Logical-Comparison-Operators/Unsolved/script.js
@@ -1,18 +1,27 @@
 var a = "50";
 var b = 50;
 var c = 100;
-var d = c % b;
+var d = c % b; // 100 / 50 = 0// -- is it divisible? if yes, 0. if not, no.
 var e = c / 2;
 
+// 50 = 100/50 //
 var expression1 = (b === e);
+// 2 < 2 //
 var expression2 = (e < d);
 
 // Use comparison operators so all expressions below log to the console as true
+// "50" = 50
 console.log(a == b);
+// 50 = 100/2 //
 console.log(b === e);
+// 100 > 50 //
 console.log(c > b);
-console.log(d === 0);
+// 100/50 == 50 //
+console.log(d === 50);
 
-// Use logical operators so all expressions below log to the console as true
-console.log( true && true);
-console.log( !false || false);
+// // Use logical operators so all expressions below log to the console as true
+// console.log( true && true);
+// console.log( !false || false);
+
+console.log( expression1 && expression2);
+console.log( !expression1 || expression2);
