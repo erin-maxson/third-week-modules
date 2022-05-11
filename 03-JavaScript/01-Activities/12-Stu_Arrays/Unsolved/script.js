5f5f4741bb41b6baa9f9025737a9c41a0334f918
erin-maxson <erin@cohatch.com>
2022-05-11 18:04:25 -0500

    adding javascript to log all info into the console

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,23 @@
 // WRITE YOUR CODE HERE
+var names = ["Erin", "Rory", "Pasta"];
+
+console.log(names);
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+
+console.log("Welcome to the class " + names[0]);
+console.log("Welcome to the class " + names[1]);
+console.log("Welcome to the class " + names[2]);
+
+names[0] = "Taura";
+names[1] = "Nimi";
+names[2] = "Teddy";
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+
+
+console.log(names.length);
