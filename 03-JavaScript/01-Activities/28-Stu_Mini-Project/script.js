bc0ddc277b146003000638c919c2040b7eb1179e
erin-maxson <erin@cohatch.com>
2022-05-14 12:33:12 -0500

    trying to get first message to return

--- 03-JavaScript/01-Activities/28-Stu_Mini-Project/script.js
+++ 03-JavaScript/01-Activities/28-Stu_Mini-Project/script.js
@@ -1,16 +1,34 @@
+var gameOptions = ["R", "P", "S"]
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
-//Give instructions to user on how to play the game w/ confirmation
+var playGame = function() {
+    var userChoice = window.alert("Welcome to the Rock, Paper, Scissors Arena. To play this game, enter R for Rock, P for Paper, or S for scissors. Would you like to play a game?");
+        return;
+    }
 
-//Get input from user if they'd like to play the game
+userChoice = userChoice.toUpperCase();
 
-//Return the option to input their options: R, P, S
+// var index - Math.floor(Math.random() ^ gameOptions.length)
+// var computerChoice = options [index];
 
-//Get input from user
+// //Give instructions to user on how to play the game w/ confirmation
+// prompt("Welcome to the Rock, Paper, Scissors Arena. To play this game, enter R for Rock, P for Paper, or S for scissors.");
+// //Get input from user if they'd like to play the game
+// prompt("Would you like to play a game of Rock, Paper, Scissors?");
+// //Return the option to input their options: R, P, S
+// var userChoice = window.prompt("Enter R, P, or S.")
+// //Get input from user
+
 
 //Run random/automated game of R, P, S
 
+
 //Generate response
+
 
 //Show the game score -- computer vs user
 
 //Ask to play again
+prompt("Would you like to play again?")
