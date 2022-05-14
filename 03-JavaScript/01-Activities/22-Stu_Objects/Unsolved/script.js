9dfa883bbca258125ed1ae807ca521c510cc77ff
erin-maxson <erin@cohatch.com>
2022-05-14 09:59:27 -0500

    working on drink order info and console logs

--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,11 +1,30 @@
 //WRITE YOUR CODE BELOW
+
+// First Drink Order Info
 var customerOrder = {
     drinkName: "London Fog",
     sugarNum: 2,
-    ready: true,
+    isReady: true,
 }
 
-//Logs drinkName //
+// Second Drink Order Info
+var customerOrder2 = {
+    drinkName: "Caramel Cowboy",
+    sugarNum: 1,
+    isReady: false,
+}
+
+//Logs 1st order drinkName //
 console.log(customerOrder.drinkName);
-// Logs # of sugars in drink //
+// Logs # of sugars in drink in 1st order //
 console.log(customerOrder.sugarNum);
+//Logs status of drink in 1st order //
+
+
+//Logs 2nd order drinkName //
+console.log(customerOrder2.drinkName);
+// Logs # of sugars in drink in 2nd order //
+console.log(customerOrder2.sugarNum);
+//Logs status of drink in 2nd order //
+
+
