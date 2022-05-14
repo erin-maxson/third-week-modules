e05e0afe502307d2e27b94a50eee44fd1f3ad346
erin-maxson <erin@cohatch.com>
2022-05-14 10:12:36 -0500

    drink order status updates w/ if and else

--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -19,6 +19,12 @@ console.log(customerOrder.drinkName);
 // Logs # of sugars in drink in 1st order //
 console.log(customerOrder.sugarNum);
 //Logs status of drink in 1st order //
+if (customerOrder.isReady === true) {
+    console.log("Ready for pick-up")}
+
+else {
+    console.log("Still in queue")
+}
 
 
 //Logs 2nd order drinkName //
@@ -26,5 +32,9 @@ console.log(customerOrder2.drinkName);
 // Logs # of sugars in drink in 2nd order //
 console.log(customerOrder2.sugarNum);
 //Logs status of drink in 2nd order //
+if (customerOrder2.isReady === true) {
+    console.log("Ready for pick-up")}
 
-
+else {
+    console.log("Still in queue")
+}
