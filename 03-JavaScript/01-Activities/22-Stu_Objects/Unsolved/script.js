5c07d92bf2efc899bd1dad6a227f7c138faddfae
erin-maxson <erin@cohatch.com>
2022-05-14 09:49:01 -0500

    setting up drink console log

--- 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ 03-JavaScript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,11 @@
 //WRITE YOUR CODE BELOW
+var customerOrder = {
+    drinkName: "London Fog",
+    sugarNum: 2,
+    ready: true,
+}
+
+//Logs drinkName //
+console.log(customerOrder.drinkName);
+// Logs # of sugars in drink //
+console.log(customerOrder.sugarNum);
