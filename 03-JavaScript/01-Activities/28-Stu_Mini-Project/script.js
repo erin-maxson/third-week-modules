46c665c1dac56e6a2f0733b46a447007a143fa94
erin-maxson <erin@cohatch.com>
2022-05-14 12:13:45 -0500

    psuedo code for script.js

--- 03-JavaScript/01-Activities/28-Stu_Mini-Project/script.js
+++ 03-JavaScript/01-Activities/28-Stu_Mini-Project/script.js
@@ -1,3 +1,16 @@
-//gameOption Returns
-var gameOptions = ["Rock", "Paper", "Scissors"];
 
+//Give instructions to user on how to play the game w/ confirmation
+
+//Get input from user if they'd like to play the game
+
+//Return the option to input their options: R, P, S
+
+//Get input from user
+
+//Run random/automated game of R, P, S
+
+//Generate response
+
+//Show the game score -- computer vs user
+
+//Ask to play again
